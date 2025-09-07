e7495346e4e95db7d72c22389190438acbf790d7
saba <saba2006.diasamidze@gmail.com>
2025-09-07 11:49:30 +0400

    refactored main to be more readable

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,56 @@
-#include<raylib.h>
-#include<deque>
-#include<iostream>
-#include"../include/window.h"
-#include"../include/snake.h"
-#include"../include/input.h"
-#include"../include/food.h"
-
-void runState();
-void idleState();
-
-Window win;
-Snake snake;
-Input input;
-Food food;
-
-int score = 0;
-bool game_state_run = false;
-std::string direction = "";
+#include <raylib.h>
+#include "../include/window.h"
+#include "../include/snake.h"
+#include "../include/input.h"
+#include "../include/food.h"
 
 int main() {
-  while( win.running() ) {
-    direction = input.getCurrentAction();
-    if( direction != "" ) {
-      snake.changeDirection(direction);
-      game_state_run = true;
-    }
-    if( game_state_run ) {
-      runState();
-    }else {
-      idleState();
-    }
-  }
-  win.close();
-}
+    Window win;
+    Snake snake;
+    Input input;
+    Food food;
 
-void runState() {
-  snake.move();
-  std::deque<Rectangle> tail = snake.getTail();
-  bool foodReachable = snake.isFoodReachable( food.getLocationX(), food.getLocationY() );
-  if( foodReachable ) {
-    food.setRandomLocation( tail );
-    score++;
-    snake.eatFood();
-  }
-  snake.checkBoundries();
-  if( snake.isDead() ) {
-    score = 0;
-    snake.reset();
-    game_state_run = false;
-    direction = "";
-  }
-  BeginDrawing();
-  win.refresh();
-  win.displayScore( score );
-  snake.draw();
-  food.draw();
-  EndDrawing();
-}
+    int score = 0;
+    bool game_state_run = false;
+    std::string direction;
 
-void idleState() {
-  BeginDrawing();
-  win.refresh();
-  snake.draw();
-  food.draw();
-  win.showStartMessage();
-  EndDrawing();
+    auto drawScene = [&](bool running) {
+        BeginDrawing();
+        win.refresh();
+        snake.draw();
+        food.draw();
+        if (running) win.displayScore(score);
+        else win.showStartMessage();
+        EndDrawing();
+    };
+
+    while (win.running()) {
+        direction = input.getCurrentAction();
+
+        if (!direction.empty()) {
+            snake.changeDirection(direction);
+            game_state_run = true;
+        }
+
+        if (game_state_run) {
+            snake.move();
+            if (snake.isFoodReachable(food.getLocationX(), food.getLocationY())) {
+                food.setRandomLocation(snake.getTail());
+                score++;
+                snake.eatFood();
+            }
+            snake.checkBoundries();
+
+            if (snake.isDead()) {
+                score = 0;
+                snake.reset();
+                game_state_run = false;
+                direction.clear();
+            }
+        }
+
+        drawScene(game_state_run);
+    }
+
+    win.close();
 }
